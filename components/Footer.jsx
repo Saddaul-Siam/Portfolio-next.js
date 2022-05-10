34ad15bc25a4cd471e8648461bff5198fd3e7495
saddaul-siam <saddadul.siam@gmail.com>
2022-05-10 13:16:04 +0600

    footer added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,23 +3,45 @@ import React from "react";
 import footerImg from "../img/wave1.svg";
 import footerShape1 from "../img/footer-shape1.png";
 import footerShape2 from "../img/footer-shape2.png";
+import {
+  FaFacebookSquare,
+  FaInstagramSquare,
+  FaLinkedin,
+  FaGithubSquare,
+} from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className="pt-[90] relative">
-      <div className="relative">
-        <Image
-          className="opacity-80"
-          layout="responsive"
-          src={footerImg}
-          alt=""
-        />
+      <div className="">
+        <div className="relative">
+          <Image
+            className="opacity-80"
+            layout="responsive"
+            src={footerImg}
+            alt=""
+          />
+        </div>
+        <div className="absolute bottom-0 left-0 animate-pulse opacity-100">
+          <Image className="absolute" height="300" src={footerShape1} alt="" />
+        </div>
+        <div className="absolute bottom-0 left-0 animate-pulse duration-[5000] opacity-100">
+          <Image className="absolute" height="300" src={footerShape2} alt="" />
+        </div>
       </div>
-      <div className="absolute bottom-0 left-0 animate-pulse opacity-100">
-        <Image className="absolute" height="300" src={footerShape1} alt="" />
-      </div>
-      <div className="absolute bottom-0 left-0 animate-pulse duration-[5000] opacity-100">
-        <Image className="absolute" height="300" src={footerShape2} alt="" />
+      <div className="absolute top-2/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center space-y-10">
+        <div className="flex space-x-10">
+          <FaGithubSquare className="text-white hover:text-black text-5xl cursor-pointer" />
+          <FaLinkedin className="text-white hover:text-blue-500 text-5xl cursor-pointer" />
+          <FaFacebookSquare className="text-white hover:text-blue-700 text-5xl cursor-pointer" />
+          <FaInstagramSquare className="text-white hover:text-pink-600 text-5xl cursor-pointer" />
+        </div>
+        <h4 className=" text-xl text-slate-00">
+          Develop by{" "}
+          <a className="underline decoration-sky-500 text-rose900">
+            Saddaul Siam
+          </a>
+        </h4>
       </div>
     </footer>
   );

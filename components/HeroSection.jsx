e1ac835376828d872b1c80fea2f25d09d964e8be
saddaul-siam <saddadul.siam@gmail.com>
2022-06-11 19:07:54 +0600

    fiexd hero section

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -15,7 +15,7 @@ import {
 
 const HeroSection = () => {
   return (
-    <header className="container md:mx-auto h-[80vh] mt-28 md:mt-48">
+    <header className="md:container md:mx-auto h-[80vh] mt-28 md:mt-48">
       <div className="flex-row md:flex mx-5 xl:mx-0">
         <div className="flex-1 space-y-7">
           <h2 className="text-5xl font-bold font-lora">Hy! I Am</h2>
@@ -38,15 +38,15 @@ const HeroSection = () => {
           </div>
         </div>
         <div className="flex-1 relative pt-32 md:pt-0">
-          <div className="absolute left-[-15%] md:top-[5%] lg:top[-9%] ">
+          <div className="absolute left-0 right-0 md:left-[-15%] top-52 md:top-[5%] lg:top[-9%] ">
             <Image src={Vector1} className="scale-[0.70] z-20" alt="" />
           </div>
-          <div className="absolute left-[-3rem] md:top-[1rem] lg:top-[-4.6rem] ">
+          <div className="absolute left-0 right-0 md:left-[-3rem] top-48 md:top-[1rem] lg:top-[-4.6rem] ">
             <Image src={Vector2} className="scale-[0.68] z-30" alt="" />
           </div>
 
           <div className="">
-            <div className="md:hidden absolute top-8 left-[3%]">
+            <div className="md:hidden absolute top-28 left-16 right-0">
               <Image src={boy} className="scale-90 z-40" alt="" />
             </div>
             <div className="hidden md:block absolute md:top-0 md:left-[20%] md:-bottom-20 ">
@@ -68,12 +68,12 @@ const HeroSection = () => {
             {/* <Image src={glassesimoji} alt="" className="scale-75" /> */}
           </div>
 
-          <div className="h-20 w-44 md:h-24 md:w-48 lg:w-64 flex justify-start items-center rounded-3xl absolute top-5 md:-top-24 -right-5 md:right-0 bg-white z-50">
+          <div className="h-20 w-44 md:h-24 md:w-48 lg:w-64 flex justify-start items-center rounded-3xl absolute top-10 md:-top-24 -right-5 md:right-0 bg-white z-50">
             <Image src={crown} alt="" className="scale-50" />
             <span className="text-lg text-gray-600">Web Developer</span>
           </div>
 
-          <div className="h-20 w-44 md:h-24 md:w-48 lg:w-64 flex justify-start items-center rounded-3xl absolute -bottom-96 left-0 md:bottom-12 md:-left-8 bg-white z-50 ">
+          <div className="h-20 w-44 md:h-24 md:w-48 lg:w-64 flex justify-start items-center rounded-3xl absolute -bottom-[28rem] left-0 md:bottom-12 md:-left-8 bg-white z-50 ">
             <Image src={thumbup} alt="" className="scale-50 " />
             <span className="text-lg text-gray-600">Web Designer</span>
           </div>
